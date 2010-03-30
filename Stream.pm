cddcc02397e8acd3846320e718eac7c85cb97225
Martin Redmond <reds@cpan.org>
2010-03-30 20:12:23 +0000

    Import of REDS/Net-Twitter-Stream-0.23 from CPAN.
    gitpan-cpan-distribution: Net-Twitter-Stream
    gitpan-cpan-version:      0.23
    gitpan-cpan-path:         REDS/Net-Twitter-Stream-0.23.tgz
    gitpan-cpan-author:       REDS
    gitpan-cpan-maturity:     released

--- Stream.pm
+++ Stream.pm
@@ -1,8 +1,11 @@
 package Net::Twitter::Stream;
 use strict;
 use warnings;
-use LWP::UserAgent;
-our $VERSION = '0.2';
+use IO::Socket;
+use MIME::Base64;
+use JSON;
+
+our $VERSION = '0.23';
 1;
 
 =head1 NAME
@@ -25,10 +28,8 @@ Using Twitter's streaming api.
 
 =head1 DESCRIPTION
 
-Twitter recently allowed access to a streaming version of their api.
-The api is currently under "alpha test" release, but hopfully it will
-prove scalable and become the normal way to suck data from the
-twitterverse.
+The Streaming verson of the Twitter API allows near-realtime access to 
+various subsets of Twitter public statuses.
 
 Recent update: Track and follow are now merged into a single api call.
 /1/status/filter.json now allows a single connection go listen for keywords
@@ -37,10 +38,12 @@ and follow a list of users.
 HTTP Basic authentication is supported (no OAuth yet) so you will need a twitter
 account to connect.
 
+JSON format is only supported. Twitter may depreciate XML.
+
 Options 
   user, pass: required, twitter account user/password
   callback: required, a subroutine called on each received tweet
-  format: optional, returned data format, json or xml, defaults to json
+  
 
 perl@redmond5.com
 @martinredmond
@@ -52,29 +55,41 @@ sub new {
   my $class = shift;
   my %args = @_;
   die "Usage: Net::Twitter::Stream->new ( user => 'user', pass => 'pass', callback => \&got_tweet_cb )" unless
-      $args{user} && $args{pass} && $args{callback};
+    $args{user} && $args{pass} && $args{callback};
   my $self = bless {};
   $self->{user} = $args{user};
   $self->{pass} = $args{pass};
   $self->{got_tweet} = $args{callback};
-  my $format = 'json';
-  $format = $args{format} if $args{format};
-
+  
   my $content = "follow=$args{follow}" if $args{follow};
   $content = "track=$args{track}" if $args{track};
   $content = "follow=$args{follow}&track=$args{track}\r\n" if $args{track} && $args{follow};
-
-  my $req = HTTP::Request->new ( 'POST', 
-				 "http://$args{user}:$args{pass}\@stream.twitter.com/1/statuses/filter.$format",
-				 [ 'Content-Type' => 'application/x-www-form-urlencoded' ],
-				 $content );
-  return $req if $args{getreq};
-
-  LWP::UserAgent->new->request ( $req,
-				 sub {
-				     my ( $chunk, $res ) = @_;
-				     $args{callback}($chunk, $res);
-				 } );
+  
+  my $auth = encode_base64 ( "$args{user}:$args{pass}" );
+  chomp $auth;
+  
+  my $cl = length $content;
+  my $req = <<EOF;
+POST /1/statuses/filter.json HTTP/1.1\r
+Authorization: Basic $auth\r
+Host: stream.twitter.com\r
+User-Agent: net-twitter-stream/0.1\r
+Content-Type: application/x-www-form-urlencoded\r
+Content-Length: $cl\r
+\r
+EOF
+  
+  my $sock = IO::Socket::INET->new ( PeerAddr => 'stream.twitter.com:80' );
+  $sock->print ( "$req$content" );
+  while ( my $l = $sock->getline ) {
+    last if $l =~ /^\s*$/;
+  }
+  while ( my $l = $sock->getline ) {
+    eval { 
+      my $o = from_json ( $l );
+      $self->{got_tweet} ( $o, $l );
+    };
+  }
 }
 
 
